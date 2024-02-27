a3488e093558898d60b07f1ae5f8daac8c356d03
Matilde Simões <up202108782@edu.fe.up.pt>
2024-02-27 10:28:50 +0000

    created tangram - ex1

--- tp2/MyInterface.js
+++ tp2/MyInterface.js
@@ -30,14 +30,14 @@ export class MyInterface extends CGFinterface {
         this.gui.add(this.scene, 'displayParallelogram').name('Display Parallelog.');
 
         //Checkbox element in GUI
-        this.gui.add(this.scene, 'displayTriangleSmall').name('Display Small');
+        this.gui.add(this.scene, 'displayTriangleBig1').name('Display Big1');
+        this.gui.add(this.scene, 'displayTriangleBig2').name('Display Big2');
 
-        //Checkbox element in GUI
-        this.gui.add(this.scene, 'displayTriangleBig').name('Display Big');
+        this.gui.add(this.scene, 'displayTriangleSmall1').name('Display Small1');
+        this.gui.add(this.scene, 'displayTriangleSmall2').name('Display Small2');
 
-        //Slider element in GUI
-        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
-
+         //Slider element in GUI
+         this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
 
         return true;
     }
--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -30,14 +30,23 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.diamond = new MyDiamond(this);
+    this.triangle = new MyTriangle(this);
+    this.paralellogram = new MyParallelogram(this);
+    this.triangleSmall1 = new MyTriangleSmall(this);
+    this.triangleSmall2 = new MyTriangleSmall(this);
+    this.triangleBig1 = new MyTriangleBig(this);
+    this.triangleBig2 = new MyTriangleBig(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
     this.displayDiamond = true;
     this.displayTriangle = true;
     this.displayParallelogram = true;
-    this.displayTriangleSmall = true;
-    this.displayTriangleBig = true;
+    this.displayTriangleSmall1 = true;
+    this.displayTriangleSmall2 = true;
+    this.displayTriangleBig1 = true;
+    this.displayTriangleBig2 = true;
+
     this.scaleFactor = 1;
   }
   initLights() {
@@ -106,7 +115,7 @@ export class MyScene extends CGFscene {
       1, 0, 0, 0,
       0, 1, 0, 0,
       0, 0, 1, 0,
-      0.9, Math.sqrt(2) + 1 , 0, 1
+      0.9, Math.sqrt(2) + 0.87 , 0, 1
     ];
 
     var rotate = [
@@ -117,36 +126,48 @@ export class MyScene extends CGFscene {
     ];
     
     this.pushMatrix();
-
     this.multMatrix(translate);
     this.multMatrix(rotate);
     if (this.displayDiamond) this.diamond.display();
-
     this.popMatrix();
 
-    if (this.displayTriangle) this.triangle.display();
+    this.pushMatrix();
+    this.translate(Math.sqrt(2), 0, 0);
+    this.rotate(Math.PI/4, 0, 0, 1);
+    if (this.displayTriangleBig1) this.triangleBig1.display();
+    this.popMatrix();
+
+    this.pushMatrix();
+    this.translate(-Math.sqrt(2), -Math.sqrt(2) ,0);
+    this.rotate(-Math.PI/4, 0, 0, 1);
+    if (this.displayTriangleBig2) this.triangleBig2.display();
+    this.popMatrix();
+
+    this.pushMatrix();
+    this.scale(1, -1, 1);
+    this.translate(-1.81, 1, 0);
+    this.rotate(Math.PI, 0, 0, 1);
     if (this.displayParallelogram) this.paralellogram.display();
-    if (this.displayTriangleSmall) this.triangleSmall.display();
-    if (this.displayTriangleBig) this.triangleBig.display();
+    this.popMatrix();
+
+    this.pushMatrix();
+    this.translate(-4.9, 0.1, 0);
+    this.rotate(3 * Math.PI/4, 0, 0, 1);
+    if (this.displayTriangleSmall1) this.triangleSmall1.display();
+    this.popMatrix();
+    
+    this.pushMatrix();
+    this.translate(-Math.sqrt(2), -2 * Math.sqrt(2), 0);
+    this.rotate(3 * Math.PI/4, 0, 0, 1);
+    if (this.displayTriangle) this.triangle.display();
+    this.popMatrix();
+
+    this.pushMatrix();
+    this.translate(-1.5, -4.3, 0);
+    this.rotate(-3 * Math.PI/4, 0, 0, 1);
+    if (this.displayTriangleSmall2) this.triangleSmall2.display();
+    this.popMatrix();
 
     // ---- END Primitive drawing section
-  }
-
-  createTranslateMatrix(x, y, z) {
-    return [
-      1, 0, 0, 0,
-      0, 1, 0, 0,
-      0, 0, 1, 0,
-      x, y, z, 1
-    ];
-  }
-
-  createRotateMatrix(angle, x, y, z) {
-    return [
-      Math.cos(angle), Math.sin(angle), 0, 0,
-      -Math.sin(angle), Math.cos(angle), 0, 0,
-      0, 0, 1, 0,
-      0, 0, 0, 1
-    ];
   }
 }
