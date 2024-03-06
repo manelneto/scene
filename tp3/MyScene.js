039069c932743389de34284f5114bbfc0b4018d6
Manuel Neto <manuelrlcneto@gmail.com>
2024-03-06 18:12:00 +0000

    added wood

--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -130,6 +130,13 @@ export class MyScene extends CGFscene {
         this.material3.setSpecular(1, 0, 0, 1.0);
         this.material3.setShininess(10.0);
 
+        // Wood (low specular)
+        this.wood = new CGFappearance(this);
+        this.wood.setAmbient(0.05, 0.05, 0.05, 1.0);
+        this.wood.setDiffuse(161/255, 102/255, 47/255, 1.0);
+        this.wood.setSpecular(0.1, 0.1, 0.1, 1.0);
+        this.wood.setShininess(10.0);
+
         // Custom material (can be changed in the interface)
         // initially midrange values on ambient, diffuse and specular, on R, G and B respectively
 
@@ -143,10 +150,10 @@ export class MyScene extends CGFscene {
 
         this.updateCustomMaterial();
 
-        this.materials = [this.material1, this.material2, this.material3, this.customMaterial];
+        this.materials = [this.material1, this.material2, this.material3, this.customMaterial, this.wood];
 
         // Labels and ID's for object selection on MyInterface
-        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Custom': 3 };
+        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Custom': 3, 'Wood': 4 };
     }
     display() {
         // ---- BEGIN Background, camera and axis setup
