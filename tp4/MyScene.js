b076d332cb7215752c74371bd05804d2a41c3809
Matilde Simões <up202108782@edu.fe.up.pt>
2024-03-12 09:48:10 +0000

    added checkbox

--- tp4/MyScene.js
+++ tp4/MyScene.js
@@ -46,6 +46,7 @@ export class MyScene extends CGFscene {
 
         //-------Objects connected to MyInterface
         this.displayAxis = true;
+        this.displayQuad = true;
         this.scaleFactor = 5;
         this.selectedTexture = -1;        
         this.wrapS = 0;
@@ -124,7 +125,8 @@ export class MyScene extends CGFscene {
         
         // this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
 
-        this.quad.display();
+        if (this.displayQuad)
+            this.quad.display();
 
         // ---- END Primitive drawing section
     }
