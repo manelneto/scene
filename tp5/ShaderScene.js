4ae5d869877a12c7b319f98290b6be47a66fc03a
Manuel Neto <manuelrlcneto@gmail.com>
2024-03-25 15:47:31 +0000

    added water.frag and water.vert

--- tp5/ShaderScene.js
+++ tp5/ShaderScene.js
@@ -90,7 +90,8 @@ export class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/sepia.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
 			new CGFshader(this.gl, "shaders/yellow_blue_bule.vert", "shaders/yellow_blue_bule.frag"),
-			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/gray.frag")
+			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/gray.frag"),
+			new CGFshader(this.gl, "shaders/water.vert", "shaders/water.frag"),
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
@@ -114,7 +115,8 @@ export class ShaderScene extends CGFscene {
 			'Sepia': 7,
 			'Convolution': 8,
 			'YellowBlue': 9,
-			'Gray': 10
+			'Gray': 10,
+			'Water': 11
 		};
 
 		// shader code panels references
