d3947258b967b5c9a58be38e9495ef7d83796cad
Manuel Neto <manuelrlcneto@gmail.com>
2024-04-20 23:25:49 +0100

    changed panorama accoridng to project description

--- project/MyPanorama.js
+++ project/MyPanorama.js
@@ -1,20 +1,39 @@
-import { CGFobject } from '../../lib/CGF.js';
+import { CGFappearance, CGFobject } from '../../lib/CGF.js';
 import { MySphere } from './MySphere.js';
 
+/**
+ * MyPanorama
+ * @constructor
+ * @param scene - Reference to MyScene object
+ * @param texture - Texture to apply to the sphere
+ */
 export class MyPanorama extends CGFobject {
-
     constructor(scene, texture) {
         super(scene);
-        this.texture = texture;
+        this.sphere = new MySphere(scene, 64, 32, false, 200);
 
-        this.sphere = new MySphere(this.scene, 64, 32, true, 200);
+        this.material = new CGFappearance(this.scene);
+        this.material.setAmbient(0, 0, 0, 0);
+        this.material.setDiffuse(0, 0, 0, 0);
+        this.material.setEmission(1, 1, 1, 1);
+        this.material.setShininess(0.0);
+        this.material.setSpecular(0, 0, 0, 0);
+        this.material.setTexture(texture);
     }
 
     display() {
-        this.texture.apply();
         this.scene.pushMatrix();
         this.scene.translate(this.scene.camera.position[0], this.scene.camera.position[1], this.scene.camera.position[2]);
+        this.material.apply();
         this.sphere.display();
         this.scene.popMatrix();
+    }
+
+    enableNormalViz() {
+        this.sphere.enableNormalViz();
+    }
+
+    disableNormalViz() {
+        this.sphere.disableNormalViz();
     }
 }
