ee99910cc61ff54d1e2295a9868e4ec63b4e790a
Manuel Neto <manuelrlcneto@gmail.com>
2024-04-20 23:26:07 +0100

    improved sphere code

--- project/MySphere.js
+++ project/MySphere.js
@@ -1,8 +1,13 @@
-import {CGFobject} from '../lib/CGF.js';
+import { CGFobject } from '../lib/CGF.js';
+
 /**
  * MySphere
  * @constructor
  * @param scene - Reference to MyScene object
+ * @param slices - Number of slices ("sides" around the Y axis)
+ * @param stacks - Number of stacks (divisions from the "equator" to the "poles")
+ * @param isInverted - Boolean to indicate if the sphere should be inverted (i.e. visible from the inside and not the outside)
+ * @param radius - Sphere radius
  */
 export class MySphere extends CGFobject {
     constructor(scene, slices, stacks, isInverted, radius) {
@@ -20,42 +25,47 @@ export class MySphere extends CGFobject {
         this.normals = [];
 		this.texCoords = [];
 
-        let phi, theta;
-        let x, y, z;
-        for (let stack = 0; stack <= this.stacks; stack++) {
-            phi = Math.PI * stack / this.stacks; 	
+        let phi;        // angle with the YY axis, from -PI/2 to PI/2
+        let theta;      // angle with the XX axis, from 0 to 2PI
+        let x, y, z;    // vertices coordinates
+        let u, v;       // texture coordinates
+        let r = this.radius;
+        for (let stack = -this.stacks; stack <= this.stacks; stack++) {
+            phi = (Math.PI / 2) * (stack / this.stacks);
             for (let slice = 0; slice <= this.slices; slice++) {
-                theta = 2 * Math.PI * slice / this.slices; 
-                x = Math.sin(phi) * Math.cos(theta);
-                y = Math.cos(phi);
-                z = Math.sin(phi) * Math.sin(theta);
-                this.vertices.push(x * this.radius, y * this.radius, z * this.radius);
+                theta = (2 * Math.PI) * (slice / this.slices);
+                
+                x = r * Math.cos(phi) * Math.cos(theta);
+                y = r * Math.sin(phi);
+                z = r * Math.cos(phi) * Math.sin(theta);
 
-                let magnitude = Math.sqrt(x * x + y * y + z * z); // TODO
+                this.vertices.push(x, y, z);
 
-				if (this.isInverted) {
-					this.normals.push(-(x / magnitude), -(y / magnitude), -(z / magnitude));
+                if (this.isInverted) {
+					this.normals.push(-x/r, -y/r, -z/r);
 				} else {
-					this.normals.push(x / magnitude, y / magnitude, z / magnitude);
+					this.normals.push(x/r, y/r, z/r);
 				}
 
-				let u = 1 - (slice / this.slices);
-				let v = stack / this.stacks;
+
+                u = 1 - (slice / this.slices);
+				v = 1/2 - (stack / (2 * this.stacks));
 				this.texCoords.push(u, v);
             }
         }
 
-        for (let stack = 0; stack <= this.stacks; stack++) {
+        let lower, upper;
+        for (let stack = 0; stack <= 2 * this.stacks; stack++) {
             for (let slice = 0; slice <= this.slices; slice++) {
-                let first = (stack * (this.slices + 1)) + slice;
-                let second = first + this.slices + 1;
+                lower = stack * (this.slices + 1) + slice;
+                upper = lower + this.slices + 1;
 
 				if (this.isInverted) {
-					this.indices.push(first, second, first + 1);
-					this.indices.push(second, second + 1, first + 1);
+					this.indices.push(lower, upper, lower + 1);
+					this.indices.push(upper, upper + 1, lower + 1);
 				} else {
-					this.indices.push(first + 1, second, first);
-					this.indices.push(first + 1, second + 1, second);
+					this.indices.push(lower + 1, upper, lower, );
+					this.indices.push(lower + 1, upper + 1, upper);
 				}
             }
         }
