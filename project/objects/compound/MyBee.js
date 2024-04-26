646c60a6869190170e5d80cfb7f3e17d392fb46d
Matilde Simões <up202108782@edu.fe.up.pt>
2024-04-26 18:50:21 +0100

    added transparencia

--- project/objects/compound/MyBee.js
+++ project/objects/compound/MyBee.js
@@ -33,13 +33,13 @@ export class MyBee extends CGFobject {
             this.legs.push(new MyLeg(this.scene));
         }
 
-        this.headMaterial = this.createMaterial([1, 1, 1], 'images/head1.png');
-        this.eyeMaterial = this.createMaterial([0.8, 0.8, 0.8], 'images/eye.png');
-        this.antennaMaterial = this.createMaterial([0.1, 0.1, 0.1], 'images/antennae.png');
-        this.thoraxMaterial = this.createMaterial([1, 1, 1], 'images/head1.png');
-        this.abdomenMaterial = this.createMaterial([1, 1, 1], 'images/abdomen.png');
-        this.wingMaterial = this.createMaterial([1, 1, 1], 'images/wing.png');
-        this.legMaterial = this.createMaterial([0.1, 0.1, 0.1], 'images/antennae.png');
+        this.headMaterial = this.createMaterial([1, 1, 1, 1.0], 'images/head1.png');
+        this.eyeMaterial = this.createMaterial([0.8, 0.8, 0.8, 1.0], 'images/eye.png');
+        this.antennaMaterial = this.createMaterial([0.1, 0.1, 0.1, 1.0], 'images/antennae.png');
+        this.thoraxMaterial = this.createMaterial([1, 1, 1, 1.0], 'images/head1.png');
+        this.abdomenMaterial = this.createMaterial([1, 1, 0, 1.0], 'images/abdomen.png');
+        this.wingMaterial = this.createMaterial([1, 1, 1, 0.2], 'images/wing.png');
+        this.legMaterial = this.createMaterial([0.1, 0.1, 0.1, 1.0], 'images/antennae.png');
 	}
 
     display() {
@@ -89,45 +89,14 @@ export class MyBee extends CGFobject {
         this.thoraxMaterial.apply();
         this.scene.translate(0, 0.2, -1);
         this.thorax.display();
-        this.scene.popMatrix();
+        this.scene.popMatrix()
 
-        // Left big wing
-        this.scene.pushMatrix();
-        this.wingMaterial.apply();
-        this.scene.translate(-1.2, 0.4, -0.7);
-        this.scene.rotate(-Math.PI - Math.PI / 2, 0, 1, 0);
-        this.leftBigWing.display();
-        this.scene.popMatrix();
-
-        // Right big wing
-        this.scene.pushMatrix();
-        this.wingMaterial.apply();
-        this.scene.translate(1.2, 0.4, -0.7);
-        this.scene.rotate(Math.PI / 2, 0, 1, 0);
-        this.rightBigWing.display();
-        this.scene.popMatrix();
-
-        // Left small wing
-        this.scene.pushMatrix();
-        this.wingMaterial.apply();
-        this.scene.translate(-1.1, 0.35, -1.2);
-        this.scene.rotate(-Math.PI - Math.PI / 2, 0, 1, 0);
-        this.leftSmallWing.display();
-        this.scene.popMatrix();
-
-        // Right small wing
-        this.scene.pushMatrix();
-        this.wingMaterial.apply();
-        this.scene.translate(1.1, 0.35, -1.2);
-        this.scene.rotate(Math.PI / 2, 0, 1, 0);
-        this.rightSmallWing.display();
-        this.scene.popMatrix();
-
+        /*
         // Legs
         this.scene.pushMatrix();
         this.legMaterial.apply();
         this.scene.rotate(Math.PI / 2, 0, 1, 0);
-        this.scene.translate(1.5, -0.5, -0.4);
+        this.scene.translate(1.65, -0.5, -0.4);
         for (let i=0; i< 3; i++) {
             this.scene.translate(-0.3, 0, 0);
             this.legs[i].display();
@@ -137,7 +106,7 @@ export class MyBee extends CGFobject {
         this.scene.pushMatrix();
         this.legMaterial.apply();
         this.scene.rotate(-Math.PI / 2, 0, 1, 0);
-        this.scene.translate(-1.5, -0.5, -0.4);
+        this.scene.translate(-1.65, -0.5, -0.4);
         for (let i=3; i< 6; i++) {
             this.scene.translate(0.3, 0, 0);
             this.legs[i].display();
@@ -151,17 +120,58 @@ export class MyBee extends CGFobject {
         this.scene.translate(0, 0.7, -2.5);
         this.abdomen.display();
         this.scene.popMatrix();
+
+        this.scene.gl.blendFunc(this.scene.gl.SRC_ALPHA, this.scene.gl.ONE_MINUS_SRC_ALPHA);
+        this.scene.gl.enable(this.scene.gl.BLEND);
+
+        // Left big wing
+        this.scene.pushMatrix();
+        this.wingMaterial.apply();
+        this.scene.translate(-1.15, 0.6, -0.7);
+        this.scene.rotate(-Math.PI - Math.PI / 2, 0, 1, 0);
+        this.scene.rotate(Math.PI / 7, 1, 0, 0);
+        this.leftBigWing.display();
+        this.scene.popMatrix();
+
+        // Right big wing
+        this.scene.pushMatrix();
+        this.wingMaterial.apply();
+        this.scene.translate(1.15, 0.6, -0.7);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.scene.rotate(-Math.PI / 7, 1, 0, 0);
+        this.rightBigWing.display();
+        this.scene.popMatrix();
+
+        // Left small wing
+        this.scene.pushMatrix();
+        this.wingMaterial.apply();
+        this.scene.translate(-1, 0.5, -1.2);
+        this.scene.rotate(-Math.PI - Math.PI / 2, 0, 1, 0);
+        this.scene.rotate(Math.PI / 7, 1, 0, 0);
+        this.leftSmallWing.display();
+        this.scene.popMatrix();
+
+        // Right small wing
+        this.scene.pushMatrix();
+        this.wingMaterial.apply();
+        this.scene.translate(1, 0.5, -1.2);
+        this.scene.rotate(Math.PI / 2, 0, 1, 0);
+        this.scene.rotate(-Math.PI / 7, 1, 0, 0);
+        this.rightSmallWing.display();
+        this.scene.popMatrix();*/
     }
 
     createMaterial(colour, texturePath) {
         let r = colour[0];
         let g = colour[1];
         let b = colour[2];
+        let alpha = colour[3];
         let texture = new CGFtexture(this.scene, texturePath);
         let material = new CGFappearance(this.scene);
-        material.setAmbient(r, g, b, 1.0);
-        material.setDiffuse(r, g, b, 1.0);
-        material.setSpecular(r, g, b, 1.0);
+        material.setAmbient(r, g, b, alpha);
+        material.setDiffuse(r, g, b, alpha);
+        material.setSpecular(r, g, b, alpha);
+        material.setEmission(r, g, b, alpha);
         material.setTexture(texture);
         material.setTextureWrap('REPEAT', 'REPEAT');
         return material;
